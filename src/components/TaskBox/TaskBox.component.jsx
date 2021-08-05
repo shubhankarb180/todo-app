7f9ec56e539587c1ab48c676aa695707c6fd510d
shubhankarb180 <shubhankarb180@gmail.com>
2021-08-05 13:59:35 +0530

    feat: Code Refractoring for Button Component and new Button style added for Completed Tasks with Conditional Rendering

--- src/components/TaskBox/TaskBox.component.jsx
+++ src/components/TaskBox/TaskBox.component.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { remove_task, mark_completed } from "../../redux/actions";
 import "../../index.css";
+import CompletedButton from "../TaskButtons/CompletedButton.component";
+import MarkCompletedButton from "../TaskButtons/MarkCompletedButton.component";
 
 const TaskBox = ({ task, taskID, completed }) => {
   const dispatch = useDispatch();
   const completeCheck = completed === true ? "completed" : "";
+  const handleCompleteTask = () => dispatch(mark_completed(taskID));
 
   return (
     <div className="flex flex-col border-blue-900 bg-yellow-300 w-5/12 m-2 md:w-1/5 xl:w-1/6 2xl:w-1/12 justify-between hover:shadow-xl rounded-md xl:my-1">
@@ -13,22 +16,11 @@ const TaskBox = ({ task, taskID, completed }) => {
         <p className={`text-center ${completeCheck}`}>{task}</p>
       </div>
       <div className="flex justify-center pb-1 xl:pb-1.5">
-        <button className="" onClick={() => dispatch(mark_completed(taskID))}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg>
-        </button>
+        {completed === true ? (
+          <CompletedButton handleCompleteTask={handleCompleteTask} />
+        ) : (
+          <MarkCompletedButton handleCompleteTask={handleCompleteTask} />
+        )}
 
         <button onClick={() => dispatch(remove_task(taskID))}>
           <svg
@@ -42,7 +34,7 @@ const TaskBox = ({ task, taskID, completed }) => {
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth={2}
-              d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
             />
           </svg>
         </button>
