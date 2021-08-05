b15fb05bbd6dd4d97b8fbee369843e71497712ab
shubhankarb180 <shubhankarb180@gmail.com>
2021-08-05 12:37:50 +0530

    style: Alignment for TaskList adjusted for mobile devices

--- src/components/TaskList/TaskList.components.jsx
+++ src/components/TaskList/TaskList.components.jsx
@@ -4,7 +4,7 @@ import TaskBox from "../TaskBox/TaskBox.component";
 const TaskList = ({tasks,listHeader}) => {
   
   return (
-    <>
+    <div className="ml-4">
      <h1 className="lg:mx-20 px-2 text-lg font-medium">{listHeader}</h1>
       <div
         className="flex flex-wrap justify-start lg:ml-24 lg:py-1"
@@ -22,7 +22,7 @@ const TaskList = ({tasks,listHeader}) => {
           );
         })}
       </div>
-    </>
+    </div>
   );
 };
 
